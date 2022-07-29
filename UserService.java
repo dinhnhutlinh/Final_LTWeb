11695d7ff165f2c5331ec8bc1e0bd6d6097a1981
nguyenhoaibao01 <76415346+nguyenhoaibao01@users.noreply.github.com>
2022-07-29 23:20:38 +0700

    Add files via upload
    up sign up/in customer

---
+++ UserService.java
@@ -0,0 +1,53 @@
+package com.nhom10.broadstore.services;
+
+import com.nhom10.broadstore.beans.User;
+import com.nhom10.broadstore.dao.ProducerDAO;
+import com.nhom10.broadstore.db.JDBIConnector;
+import com.nhom10.broadstore.dao.UserDAO;
+import com.nhom10.broadstore.emun.Role;
+import org.jdbi.v3.core.Jdbi;
+
+import java.security.GeneralSecurityException;
+
+public class UserService {
+
+    Jdbi connector = JDBIConnector.get();
+
+    public User login(String email, String password) throws GeneralSecurityException {
+        return connector.withExtension(UserDAO.class, handle -> {
+            User user;
+
+            if ((user = handle.loginAdmin(email, password)) != null) {
+                user.setRole(Role.ADMIN);
+                return user;
+            }
+
+            String hashPassCustomer = PasswordHash.createHash(password);
+            user = handle.loginCustomer(email, hashPassCustomer);
+            if (user != null)
+                user.setRole(Role.CUSTOMER);
+
+            return user;
+        });
+    }
+
+    public int signUp(User user) {
+//
+        //
+        return connector.withExtension(UserDAO.class, handle -> handle.signUpCustomer(user));
+    }
+
+    public boolean isContainUserWithEmail(String email) {
+        return connector.withExtension(UserDAO.class, handle -> {
+            String id;
+
+            id = handle.getAdminIDWithMail(email);
+            if (id != null) {
+                return true;
+            }
+            id = handle.getCustomerIDWithMail(email);
+            return id != null;
+        });
+    }
+
+}
