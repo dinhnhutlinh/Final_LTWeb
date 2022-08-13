5c94af0e50fa5faf616c989c9b2b3f69518194b7
Đinh Nhựt Linh <79819340+dinhnhutlinh@users.noreply.github.com>
2022-08-13 22:19:38 +0700

    admin change page

---
+++ src/main/java/com/nhom10/broadstore/controllers/customerController/SearchController.java
@@ -0,0 +1,28 @@
+package com.nhom10.broadstore.controllers.customerController;
+
+import com.nhom10.broadstore.beans.Product;
+import com.nhom10.broadstore.services.ProductService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/SearchController")
+public class SearchController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String keyword = req.getParameter("keyword");
+        ProductService productService = new ProductService();
+        List<Product> findOut = productService.searchByName(keyword);
+
+        req.setAttribute("keyword", keyword);
+        req.setAttribute("findOut", findOut);
+        req.setAttribute("total", findOut.size());
+
+
+    }
+}
