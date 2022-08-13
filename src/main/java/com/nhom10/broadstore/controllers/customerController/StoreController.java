4878fa8f459cc4e7c998b760e5077cb8f3dd02ea
Đinh Nhựt Linh <79819340+dinhnhutlinh@users.noreply.github.com>
2022-08-13 22:19:38 +0700

    admin change page

--- src/main/java/com/nhom10/broadstore/controllers/customerController/StoreController.java
+++ src/main/java/com/nhom10/broadstore/controllers/customerController/StoreController.java
@@ -23,30 +23,30 @@ public class StoreController extends HttpServlet {
         String keyWord = req.getParameter("search");
 
 
-        if (cat.equals("all"))
-            req.setAttribute("products", ProductService.getInstance().getTop9());
-        else if (cat.equals("family"))
-            req.setAttribute("products", ProductService.getInstance().getProductFamilyGame());
-        else if (cat.equals("card"))
-            req.setAttribute("products", ProductService.getInstance().getProductCardGame());
-        else if (cat.equals("combat"))
-            req.setAttribute("products", ProductService.getInstance().getProductCombat());
-        else if (cat.equals("art"))
-            req.setAttribute("products", ProductService.getInstance().getProductArt());
-        else if (cat.equals("adventure"))
-            req.setAttribute("products", ProductService.getInstance().getProductAdventure());
-        else if (cat.equals("dice"))
-            req.setAttribute("products", ProductService.getInstance().getProductDice());
-        else if (cat.equals("battle"))
-            req.setAttribute("products", ProductService.getInstance().getProductBattleRoyale());
-        else if (cat.equals("fighting"))
-            req.setAttribute("products", ProductService.getInstance().getProductFighting());
-        else if (cat.equals("puzzle"))
-            req.setAttribute("products", ProductService.getInstance().getProductPuzzle());
-        else if (cat.equals("party"))
-            req.setAttribute("products", ProductService.getInstance().getProductPartyGame());
-        else if (cat.equals("fantasy"))
-            req.setAttribute("products", ProductService.getInstance().getProductFantasy());
+//        if (cat.equals("all"))
+//            req.setAttribute("products", ProductService.getInstance().getTop9());
+//        else if (cat.equals("family"))
+//            req.setAttribute("products", ProductService.getInstance().getProductFamilyGame());
+//        else if (cat.equals("card"))
+//            req.setAttribute("products", ProductService.getInstance().getProductCardGame());
+//        else if (cat.equals("combat"))
+//            req.setAttribute("products", ProductService.getInstance().getProductCombat());
+//        else if (cat.equals("art"))
+//            req.setAttribute("products", ProductService.getInstance().getProductArt());
+//        else if (cat.equals("adventure"))
+//            req.setAttribute("products", ProductService.getInstance().getProductAdventure());
+//        else if (cat.equals("dice"))
+//            req.setAttribute("products", ProductService.getInstance().getProductDice());
+//        else if (cat.equals("battle"))
+//            req.setAttribute("products", ProductService.getInstance().getProductBattleRoyale());
+//        else if (cat.equals("fighting"))
+//            req.setAttribute("products", ProductService.getInstance().getProductFighting());
+//        else if (cat.equals("puzzle"))
+//            req.setAttribute("products", ProductService.getInstance().getProductPuzzle());
+//        else if (cat.equals("party"))
+//            req.setAttribute("products", ProductService.getInstance().getProductPartyGame());
+//        else if (cat.equals("fantasy"))
+//            req.setAttribute("products", ProductService.getInstance().getProductFantasy());
 
         req.getRequestDispatcher("store.jsp").forward(req, resp);
     }
